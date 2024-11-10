6f714e151a263636f3c9ac40f5cc94b6cfaf11a7
ahmed <9638048+ahmedak@users.noreply.github.com>
2024-11-10 14:55:17 +0000

    Remove unused examples

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -1,10 +1,9 @@
 import { Resource } from "sst";
 import { Handler } from "aws-lambda";
-import { Example } from "@scandi/core/example";
 
 export const handler: Handler = async (_event) => {
   return {
     statusCode: 200,
-    body: `${Example.hello()} Linked to ${Resource.MyBucket.name}.`,
+    body: `Hello world!`,
   };
 };
