8a4b113681e24eb0594f5319cbbf220f189fe069
ahmed <9638048+ahmedak@users.noreply.github.com>
2024-11-11 06:44:40 +0000

    Create image upload form

---
+++ packages/frontend/components/form.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import styles from "./form.module.css";
+
+export default function Form({ url }: { url: string }) {
+  return (
+    <form
+      className={styles.form}
+      onSubmit={async (e) => {
+        e.preventDefault();
+
+        const file = (e.target as HTMLFormElement).file.files?.[0] ?? null;
+
+        const response = await fetch(url, {
+          body: file,
+          method: "PUT",
+          headers: {
+            "Content-Type": file.type,
+            "Content-Disposition": `attachment; filename="${file.name}"`,
+          },
+        });
+        console.log(response);
+      }}
+    >
+      <input name="file" type="file" accept="image/png, image/jpeg" />
+      <button type="submit">Upload</button>
+    </form>
+  );
+}
