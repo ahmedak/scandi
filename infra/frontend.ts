7a239e44cd3702894bf7cebb59296c714e5c21bb
ahmed <9638048+ahmedak@users.noreply.github.com>
2024-11-11 14:54:53 +0000

    Update config for deployment

--- infra/frontend.ts
+++ infra/frontend.ts
@@ -2,5 +2,5 @@ import { bucket } from "./storage";
 
 export const frontend = new sst.aws.Nextjs("frontend", {
     link: [bucket],
-    path: "./packages/frontend/",
+    path: "packages/frontend/",
   });
